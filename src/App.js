255d0e34583080013c5d2eaeec068edb536e1dd1
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-23 00:18:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>welcome to gym application.</div>;
 }
 
 export default App;
+
+// const axios = require("axios");
+
+// const options = {
+//   method: "GET",
+//   url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+//   headers: {
+//     "X-RapidAPI-Key": "d009bab2e3mshecc8f6ccb9292e6p100349jsn853191c7ec73",
+//     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+//   },
+// };
+
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
